06f9c2a0070b522d0f04b0fc6088035872f1d647
Gayatri Suslade <gayatrisuslade@outlook.com>
2020-11-05 22:49:39 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project1';
+  users=[
+    {
+      name:' Name : Gayatri Suslade ',
+      age:' Age   : 22',
+      city:' City : Kalyan'
+
+    },
+    {
+      name:'Name : Akshay  Suslade',
+      age:' Age : 25',
+      city:' City : Mumbai'
+
+    },
+    {
+      name:'Name : Bharti Suslade',
+      age:'Age : 46',
+      city:'City : Ulhasnagar'
+
+    },
+    {
+      name:'Name : Ravindra Suslade',
+      age:'Age : 50',
+      city:'City : Nashik'
+
+    }
+  ]
 }
